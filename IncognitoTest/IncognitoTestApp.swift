3e801442fb1e66fa8dd33e3ff6e931ff713b97ce
shomil03 <shomil02020@gmail.com>
2024-08-06 21:14:00 +0530

    Initial Commit

---
+++ IncognitoTest/IncognitoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncognitoTestApp.swift
+//  IncognitoTest
+//
+//  Created by Shomil Singh on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct IncognitoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
